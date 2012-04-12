039c1cf829d6bcb25907e8820ff76e13430f042a
calebphillips <caleb.phillips@gmail.com>
2012-04-12 16:41:15 -0500

    Move middle into separate namespace

--- src/square_middleware/core.clj
+++ src/square_middleware/core.clj
@@ -1,44 +1,7 @@
 (ns square-middleware.core
-  (:use compojure.core)
-  (:require [compojure.handler :as handler]))
-
-(def window-size 10)
-(defn full-window? [r] (= window-size (count r)))
-
-(defn avg [s]
-  (/ (reduce + s) (count s)))
-
-(defn variance [s mean]
-  (avg
-   (map (fn [x] (let [d (- x mean)] (* d d))) s)))
-
-
-(def responses (atom []
-                     :validator #(<= (count %) window-size)))
-
-(defn log-stats [responses]
-  (let [a (double (avg responses))
-        v (double (variance responses a))]
-    (println (str "Average: " a " Variance: " v))))
-
-(add-watch
- responses
- :responses-change
- (fn [_ _ _ new-value]
-   (when (full-window? new-value)
-     (log-stats new-value))))
-
-(defn add-response [responses new-response]
-  (let [v (if (full-window? responses)
-            (subvec responses 1)
-            responses)]
-    (conj v new-response)))
-
-(defn trunc-to-hundreds [n]
-  (-> n (quot 100) (* 100)))
-
-(defn record-response [new-response]
-  (swap! responses add-response (trunc-to-hundreds new-response)))
+  (:use [compojure.core :only [ANY]])
+  (:require [compojure.handler :as handler])
+  (:require [square-middleware.middleware :as mw]))
 
 (def statuses (mapcat #(apply range %) [[200 207] [300 307] [400 417] [500 505]]))
 (def rand-status #(rand-nth statuses))
@@ -50,13 +13,7 @@
 (defroutes main-routes
   (ANY "/locations/:id" [id] (create-response id)))
 
-(defn wrap-status-logger [handler]
-  (fn [request]
-    (let [response (handler request)]
-      (record-response (:status response))
-      response)))
-
 (def app
   (->
    (handler/api main-routes)
-   (wrap-status-logger)))
+   (mw/wrap-status-logger)))
---
+++ src/square_middleware/middleware.clj
@@ -0,0 +1,44 @@
+(ns square-middleware.middleware)
+
+(def window-size 10)
+(defn full-window? [r] (= window-size (count r)))
+
+(defn avg [s]
+  (/ (reduce + s) (count s)))
+
+(defn variance [s mean]
+  (avg
+   (map (fn [x] (let [d (- x mean)] (* d d))) s)))
+
+(def responses (atom []
+                     :validator #(<= (count %) window-size)))
+
+(defn log-stats [responses]
+  (let [a (double (avg responses))
+        v (double (variance responses a))]
+    (println (str "Average: " a " Variance: " v))))
+
+(add-watch
+ responses
+ :responses-change
+ (fn [_ _ _ new-value]
+   (when (full-window? new-value)
+     (log-stats new-value))))
+
+(defn add-response [responses new-response]
+  (let [v (if (full-window? responses)
+            (subvec responses 1)
+            responses)]
+    (conj v new-response)))
+
+(defn trunc-to-hundreds [n]
+  (-> n (quot 100) (* 100)))
+
+(defn record-response [new-response]
+  (swap! responses add-response (trunc-to-hundreds new-response)))
+
+(defn wrap-status-logger [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (record-response (:status response))
+      response)))
