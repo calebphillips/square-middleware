d1d2cd82d86c2de00e72d244dfdcf28be5e460d6
calebphillips <caleb.phillips@gmail.com>
2012-04-12 15:24:30 -0500

    Basic windowed stats and beginning of service

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject square-middleware "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [compojure "1.0.1"]]
+  :plugins [[lein-ring "0.6.3"]]
+  :ring {:handler square-middleware.core/app})
--- src/square_middleware/core.clj
+++ src/square_middleware/core.clj
@@ -1,13 +1,53 @@
-(ns square-middleware.core)
+(ns square-middleware.core
+  (:use compojure.core)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]))
 
-(defn update-cma [{current-avg :avg i :i} new-data-pt]
-  (let [sample-num (inc i)
-        new-avg (+ current-avg
-                   (/ (- new-data-pt current-avg)
-                      sample-num))]
-    {:i sample-num :avg new-avg}))
+(def window-size 10)
+(defn full-window? [r] (= window-size (count r)))
 
-(def current-cma (atom {:i 0 :avg 0}))
+(defn avg [s]
+  (/ (reduce + s) (count s)))
 
-(swap! current-cma update-cma 200)
+(defn variance [s mean]
+  (avg
+   (map (fn [x] (let [d (- x mean)] (* d d))) s)))
 
+
+(def responses (atom []
+                     :validator #(<= (count %) window-size)))
+
+(defn log-stats [responses]
+  (let [a (avg responses)
+        v (variance responses a)]
+    (println (str "Average: " a " Variance: " v))))
+
+(add-watch
+ responses
+ :responses-change
+ (fn [_ _ _ new-value]
+   (when (full-window? new-value)
+     (log-stats new-value))))
+
+(defn add-response [responses new-response]
+  (let [v (if (full-window? responses)
+            (subvec responses 1)
+            responses)]
+    (conj v new-response)))
+
+(defn record-response [new-response]
+  (swap! responses add-response new-response))
+
+(defroutes main-routes
+  (GET "/:id" [id] (str "<h1>Hello World Wide Web!</h1><h2>It's " id "</h2>")))
+
+(defn wrap-count-status [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (record-response (:status response))
+      response)))
+
+(def app
+  (->
+   (handler/site main-routes)
+   (wrap-count-status)))
