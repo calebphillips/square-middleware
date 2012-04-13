e132ff50e91820499d9b028cab8b9cfe98fb76ff
calebphillips <caleb.phillips@gmail.com>
2012-04-12 20:37:08 -0500

    Make server ssl only

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject square-middleware "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [compojure "1.0.1"]]
+                 [compojure "1.0.1"]
+                 [ring/ring-jetty-adapter "0.3.10"]]
   :plugins [[lein-ring "0.6.3"]]
-  :ring {:handler square-middleware.core/app})
+  :ring {:handler square-middleware.core/app}
+  :main square-middleware.core)
--- src/square_middleware/core.clj
+++ src/square_middleware/core.clj
@@ -1,7 +1,9 @@
 (ns square-middleware.core
-  (:use [compojure.core :only [ANY]])
-  (:require [compojure.handler :as handler])
-  (:require [square-middleware.middleware :as mw]))
+  (:use [compojure.core :only [defroutes ANY]])
+  (:require [compojure.handler :as handler]
+            [square-middleware.middleware :as mw]
+            [ring.adapter.jetty :as ring])
+  (:gen-class))
 
 (def statuses (mapcat #(apply range %) [[200 207] [300 307] [400 417] [500 505]]))
 (def rand-status #(rand-nth statuses))
@@ -17,3 +19,19 @@
   (->
    (handler/api main-routes)
    (mw/wrap-status-logger)))
+
+(def ssl-port 8443)
+
+;; Found this here: http://practice.kokonino.net/posts/ring-with-ssl-only
+(defn remove-non-ssl-connectors [server]
+  (doseq [c (filter identity (.getConnectors server))]
+    (when (not= ssl-port (.getPort c))
+      (.removeConnector server c))))
+
+(defn -main []
+  (ring/run-jetty
+   (var app)
+   {:ssl? true
+    :ssl-port ssl-port
+    :keystore "./keystore"
+    :configurator remove-non-ssl-connectors}))
