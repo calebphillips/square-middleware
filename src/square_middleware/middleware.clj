68a34e668c6591116cd652bfbba638c39fc0f035
calebphillips <caleb.phillips@gmail.com>
2012-04-12 21:21:22 -0500

    Replace 'response' with 'status'

--- src/square_middleware/middleware.clj
+++ src/square_middleware/middleware.clj
@@ -10,35 +10,36 @@
   (avg
    (map (fn [x] (let [d (- x mean)] (* d d))) s)))
 
-(def responses (atom []
+(def status-codes (atom []
                      :validator #(<= (count %) window-size)))
-
-(defn log-stats [responses]
-  (let [a (avg responses)
-        v (variance responses a)]
-    (println (str "Average: " a " Variance: " v))))
-
-(add-watch
- responses
- :responses-change
- (fn [_ _ _ new-value]
-   (when (full-window? new-value)
-     (log-stats new-value))))
-
-(defn add-response [responses new-response]
-  (let [v (if (full-window? responses)
-            (subvec responses 1)
-            responses)]
-    (conj v new-response)))
 
 (defn trunc-to-hundreds [n]
   (-> n (quot 100) (* 100)))
 
-(defn record-response [new-response]
-  (swap! responses add-response (trunc-to-hundreds new-response)))
+(defn log-stats [status-codes]
+  (let [truncd (map trunc-to-hundreds status-codes)
+        a (avg truncd)
+        v (variance truncd a)]
+    (println (str "Average: " a " Variance: " v))))
+
+(add-watch
+ status-codes
+ :status-codes-change
+ (fn [_ _ _ new-value]
+   (when (full-window? new-value)
+     (log-stats new-value))))
+
+(defn add-status [status-codes new-status]
+  (let [v (if (full-window? status-codes)
+            (subvec status-codes 1)
+            status-codes)]
+    (conj v new-status)))
+
+(defn record-status [new-status]
+  (swap! status-codes add-status new-status))
 
 (defn wrap-status-logger [handler]
   (fn [request]
     (let [response (handler request)]
-      (record-response (:status response))
+      (record-status (:status response))
       response)))
