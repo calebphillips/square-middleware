c839c308cb3aaedd3caefcd1450ccb00fbc59484
calebphillips <caleb.phillips@gmail.com>
2012-04-13 09:31:42 -0500

    Comments, cleanup, sample requests

---
+++ unauthed-request.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -I --insecure https://localhost:8443/locations/123
---
+++ unsupport-method-request.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -I -X POST --insecure --user fred:flintstone https://localhost:8443/locations/123
